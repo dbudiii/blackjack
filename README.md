5049e650f4de828ef67f6fb23fed1b56a1c87cc3
Daniel <60996796+dbudiii@users.noreply.github.com>
2024-11-16 20:12:52 -0800

    Create deck

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # blackjack
 
-"Hello World"
+Step 1: Set game items and mechanics
+ Items:
+ - Dealer
+ - Player
+ - Deck of Cards
+
+ Mechanics:
+ - Counting the deck
+ - Shuffling the deck
+ - Game flow 
+ - Hitting or passing
--- script.py
+++ script.py
@@ -1,1 +1,14 @@
 # Blackjack Script
+
+
+# Creating the deck 
+suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+deck = []
+
+def create_deck():
+    for suit in suits:
+        for rank in ranks:
+            deck.append([rank, suit])
+
+create_deck()
